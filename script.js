ed0d10d30bd4d27aa3cc841700788fa6e623cc86
arsen-melk <arsenmelk001@gmail.com>
2022-11-09 13:09:19 +0400

    triangle with while

--- script.js
+++ script.js
@@ -1,9 +1,15 @@
 
-i=22
+/* i=22
 while (i<87) {
     i=i+1
     console.log(i);
 }
 let x=2;
 let y=12;
-console.log(Math.pow(x,y));
+console.log(Math.pow(x,y)); */
+
+let line = "*";
+while (line.length<=7){
+    console.log(line + "\n");
+    line=line + "*"
+}
